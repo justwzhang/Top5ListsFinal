a5d84dfdb2bba45ab5584369292bfa3a53246542
Justin Zhang <justin.zhang.1@stonybrook.edu>
2021-11-25 22:16:31 -0500

    first commit

---
+++ client/src/auth/index.js
@@ -0,0 +1,175 @@
+import React, { createContext, useEffect, useState } from "react";
+import { useHistory } from 'react-router-dom'
+import api from '../api'
+
+const AuthContext = createContext();
+console.log("create AuthContext: " + AuthContext);
+
+// THESE ARE ALL THE TYPES OF UPDATES TO OUR AUTH STATE THAT CAN BE PROCESSED
+export const AuthActionType = {
+    GET_LOGGED_IN: "GET_LOGGED_IN",
+    REGISTER_USER: "REGISTER_USER",
+    LOGIN_USER: "LOGIN_USER",
+    SET_ERROR: "SET_ERROR",
+    LOGOUT_USER: "LOGOUT_USER",
+}
+
+function AuthContextProvider(props) {
+    const [auth, setAuth] = useState({
+        user: null,
+        loggedIn: false,
+        errorMessage: null,
+        error: false
+    });
+    const history = useHistory();
+
+    useEffect(() => {
+        if(auth.loggedIn)
+            auth.getLoggedIn();
+    }, []);
+
+    const authReducer = (action) => {
+        const { type, payload } = action;
+        switch (type) {
+            case AuthActionType.GET_LOGGED_IN: {
+                return setAuth({
+                    user: payload.user,
+                    loggedIn: payload.loggedIn,
+                    errorMessage: null,
+                    error: false
+
+                });
+            }
+            case AuthActionType.REGISTER_USER: {
+                return setAuth({
+                    user: payload.user,
+                    loggedIn: true,
+                    errorMessage: null,
+                    error: false
+                })
+            }
+            case AuthActionType.LOGIN_USER:{
+                return setAuth({
+                    user: payload.user,
+                    loggedIn: true,
+                    errorMessage: null,
+                    error: false
+                })
+            }
+            case AuthActionType.SET_ERROR:{
+                return setAuth({
+                    user:auth.user,
+                    loggedIn: auth.loggedIn,
+                    errorMessage: payload.errorMessage,
+                    error: payload.error
+                })
+            }
+            case AuthActionType.LOGOUT_USER:{
+                return setAuth({
+                    user:null,
+                    loggedIn: false,
+                    errorMessage: null,
+                    error:false
+                });
+            }
+            default:
+                return auth;
+        }
+    }
+
+    auth.logoutUser = function(){
+        let asyncLogout = async function(){
+            const response = api.logoutUser();
+            authReducer({
+                type: AuthActionType.LOGOUT_USER,
+                payload: null
+            })
+        }
+        asyncLogout();
+    }
+
+    auth.removeError = function(){
+        authReducer({
+            type: AuthActionType.SET_ERROR,
+            payload: {
+                errorMessage: null,
+                error: false
+            }
+        })
+    }
+
+    auth.logInUser = function(user, store){
+        let asyncLogInUser = async function(userInfo){
+            try{
+                const response = await api.loginUser(userInfo);
+                if (response.status === 200){
+                    authReducer({
+                        type:AuthActionType.LOGIN_USER,
+                        payload:{
+                            user:response.data.user
+                        }
+                    })
+                    history.push("/");
+                    store.loadIdNamePairs();
+                }
+            }catch(err){
+                authReducer({
+                    type: AuthActionType.SET_ERROR,
+                    payload: {
+                        errorMessage: err.response.data.errorMessage,
+                        error: true
+                    }
+                });
+            }
+        }
+        asyncLogInUser(user)
+    }
+
+    auth.getLoggedIn = async function () {
+        const response = await api.getLoggedIn();
+        if (response.status === 200) {
+            authReducer({
+                type: AuthActionType.SET_LOGGED_IN,
+                payload: {
+                    loggedIn: response.data.loggedIn,
+                    user: response.data.user
+                }
+            });
+        }
+    }
+
+    auth.registerUser = async function(userData, store) {      
+        try{
+            const response = await api.registerUser(userData);
+            if (response.status === 200) {
+                authReducer({
+                    type: AuthActionType.REGISTER_USER,
+                    payload: {
+                        user: response.data.user
+                    }
+                })
+                history.push("/");
+                store.loadIdNamePairs();
+            }
+        }catch(err){
+            authReducer({
+                type: AuthActionType.SET_ERROR,
+                payload: {
+                    errorMessage: err.response.data.errorMessage,
+                    error:true
+                }
+            });
+        }
+    }
+
+    return (
+        <AuthContext.Provider value={{
+            auth
+        }}>
+            {props.children}
+        </AuthContext.Provider>
+    );
+}
+
+export default AuthContext;
+export { AuthContextProvider };
---
+++ client/src/components/Top5Item.js
@@ -0,0 +1,135 @@
+import { React, useContext, useState } from "react";
+import { GlobalStoreContext } from '../store'
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import ListItem from '@mui/material/ListItem';
+import IconButton from '@mui/material/IconButton';
+import EditIcon from '@mui/icons-material/Edit';
+/*
+    This React component represents a single item in our
+    Top 5 List, which can be edited or moved around.
+    
+    @author McKilla Gorilla
+*/
+function Top5Item(props) {
+    const { store } = useContext(GlobalStoreContext);
+    const [editActive, setEditActive] = useState(false);
+    const [draggedTo, setDraggedTo] = useState(0);
+    const [newText, setText] = useState(props.text);
+
+    function handleDragStart(event, targetId) {
+        event.dataTransfer.setData("item", targetId);
+    }
+
+    function handleDragOver(event) {
+        event.preventDefault();
+        setDraggedTo(true);
+    }
+
+    function handleDragEnter(event) {
+        event.preventDefault();
+        console.log("entering");
+        // setDraggedTo(true);
+    }
+
+    function handleDragLeave(event) {
+        event.preventDefault();
+        console.log("leaving");
+        setDraggedTo(false);
+    }
+
+    function handleDrop(event, targetId) {
+        event.preventDefault();
+        
+        let sourceId = event.dataTransfer.getData("item");
+        sourceId = sourceId.substring(sourceId.indexOf("-") + 1);
+        if(sourceId != targetId){
+            console.log("handleDrop (sourceId, targetId): ( " + sourceId + ", " + targetId + ")");
+
+            // UPDATE THE LIST
+            store.addMoveItemTransaction(sourceId, targetId);
+        }
+        setDraggedTo(false);
+    }
+    function handleToggleEdit(){
+        setEditActive(true);
+        store.itemEditActive(true);
+    }
+    function handleKeyPress(event) {
+        if (event.code === "Enter") {
+            if(newText !== props.text){
+                store.addUpdateItemTransaction(props.index, newText);
+            }
+            setEditActive(false);
+            store.itemEditActive(false);
+        }
+    }
+    function handleUpdateText(event) {
+        setText(event.target.value);
+    }
+    
+    let { index } = props;
+
+    let itemClass = "top5-item";
+    if (draggedTo) {
+        itemClass = "top5-item-dragged-to";
+    }
+
+    let ItemElement = <ListItem
+                id={'item-' + (index+1)}
+                key={props.key}
+                className={itemClass}
+                onDragStart={(event) => {
+                    handleDragStart(event, (index+1))
+                }}
+                onDragOver={(event) => {
+                    handleDragOver(event, (index+1))
+                }}
+                onDragEnter={(event) => {
+                    handleDragEnter(event, (index+1))
+                }}
+                onDragLeave={(event) => {
+                    handleDragLeave(event, (index+1))
+                }}
+                onDrop={(event) => {
+                    handleDrop(event, (index+1))
+                }}
+                draggable="true"
+                sx={{ display: 'flex', p: 1, }}
+                style={{
+                    fontSize: '48pt',
+                    width: '100%'
+                }}
+            >
+            <Box sx={{ p: 1 }}>
+                <IconButton aria-label='edit' onClick={handleToggleEdit}>
+                    <EditIcon style={{fontSize:'48pt'}}  />
+                </IconButton>
+            </Box>
+                <Box sx={{ p: 1, flexGrow: 1 }}>{props.text}</Box>
+            </ListItem>
+
+
+if (editActive) {
+    ItemElement =
+        <TextField
+            margin="normal"
+            required
+            fullWidth
+            id={"item-" + (index+1)}
+            label="Top 5 List item"
+            name="name"
+            autoComplete="Top 5 List item"
+            className='list-card'
+            onKeyPress={handleKeyPress}
+            onChange={handleUpdateText}
+            defaultValue={props.text}
+            inputProps={{style: {fontSize: 48}}}
+            InputLabelProps={{style: {fontSize: 24}}}
+            autoFocus
+        />
+}
+    return ItemElement;
+}
+
+export default Top5Item;
