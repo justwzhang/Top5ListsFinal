204f153e5bd10dea7f088ff83e3ddcddd7680193
Justin Zhang <justin.zhang.1@stonybrook.edu>
2021-11-30 01:41:49 -0500

    Created basic home page w/o functionality

--- client/src/components/HomeScreen.js
+++ client/src/components/HomeScreen.js
@@ -4,7 +4,15 @@ import ListCard from './ListCard.js'
 import { Fab, Typography } from '@mui/material'
 import AddIcon from '@mui/icons-material/Add';
 import List from '@mui/material/List';
+import IconButton from '@mui/material/IconButton';
+import HomeIcon from '@mui/icons-material/Home';
+import PeopleIcon from '@mui/icons-material/People';
+import PersonIcon from '@mui/icons-material/Person';
+import TextField from '@mui/material/TextField';
+import FunctionsIcon from '@mui/icons-material/Functions';
+import MenuIcon from '@mui/icons-material/Menu';
 import {DeleteModal} from '.';
+import {Statusbar} from '.';
 /*
     This React component lists all the top5 lists in the UI.
     
@@ -37,23 +45,44 @@ const HomeScreen = () => {
     }
     return (
         <div id="top5-list-selector">
-            <div id="list-selector-heading">
-            <DeleteModal />
-            <Fab 
-                color="primary" 
-                aria-label="add"
-                id="add-list-button"
-                onClick={handleCreateNewList}
-            >
-                <AddIcon />
-            </Fab>
-                <Typography variant="h2">Your Lists</Typography>
+           <div id="list-selector-heading-left">
+               <IconButton>
+                    <HomeIcon style ={{
+                    fontSize: "35pt"
+                }}/>
+               </IconButton>
+               <IconButton>
+                   <PeopleIcon style ={{
+                    fontSize: "35pt"
+                }}/>
+               </IconButton>
+               <IconButton>
+                   <PersonIcon style ={{
+                    fontSize: "35pt"
+                }}/>
+               </IconButton>
+               <IconButton>
+                   <FunctionsIcon style ={{
+                    fontSize: "35pt"
+                }}/>
+               </IconButton>
+               <TextField label="Search" size="small" style ={{width: 600, backgroundColor:"white", top: "15%"}}></TextField>
+        
+            </div>
+            <div id="list-selector-heading-right">
+                Sort By
+                <IconButton>
+                   <MenuIcon style ={{
+                    fontSize: "35pt"
+                }}/>
+               </IconButton>
             </div>
             <div id="list-selector-list">
                 {
                     listCard
                 }
             </div>
+            {/* <Statusbar /> */}
         </div>)
 }
 
--- client/src/components/Statusbar.js
+++ client/src/components/Statusbar.js
@@ -1,6 +1,9 @@
 import { useContext } from 'react'
 import { GlobalStoreContext } from '../store'
-import { Typography } from '@mui/material'
+import AuthContext from '../auth'
+import {Fab, Typography } from '@mui/material'
+import IconButton from '@mui/material/IconButton';
+import AddIcon from '@mui/icons-material/Add';
 
 /*
     Our Status bar React component goes at the bottom of our UI.
@@ -9,13 +12,39 @@ import { Typography } from '@mui/material'
 */
 function Statusbar() {
     const { store } = useContext(GlobalStoreContext);
+    const { auth } = useContext(AuthContext);
+    function handleCreateNewList() {
+        store.createNewList();
+    }
     let text ="";
     if (store.currentList)
         text = store.currentList.name;
-    return (
-        <div id="top5-statusbar">
-            <Typography variant="h4">{text}</Typography>
+    
+    let statusbar = <div id="top5-statusbar"></div>
+    if(auth.loggedIn){
+        statusbar = <div id="top5-statusbar">
+            <IconButton 
+            color="primary" 
+            aria-label="add"
+            id="add-list-button"
+            onClick={handleCreateNewList}
+            style = {{
+                backgroundColor: "transparent",
+                color: "black"
+                // opacity: "0.1"
+            }}
+            >
+                <AddIcon  style ={{
+                    fontSize: "40pt"
+                }}/>
+            </IconButton>
+            Your Lists
         </div>
+    }else{
+        statusbar = <div></div>
+    }
+    return (
+        statusbar
     );
 }
 
